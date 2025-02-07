fea78b8ba2ec8ade33ff5ba8743e256175311fb7
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2025-02-07 19:27:18 -0300

    Adicionar arquivos de projeto.

---
+++ MyFirstAPI/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Communication.Requests;
+using MyFirstAPI.Communication.Responses;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}")] //para parametro na rota, ex: /user/1/orrana. Sem isso fica /user?age=1&name=orrana
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public IActionResult GetUser([FromRoute] int id)
+        {
+            var user = new User
+            {
+                Id = 1,
+                Name = "Orrana"
+            };
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult CreateUser([FromBody] RequestRegisterUserJson request)
+        {
+            var response = new ResponseRegisteredUserJson
+            {
+                Id = 1,
+                Name = request.Name,
+            };
+
+            return Created(string.Empty, response);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult UpdateUser([FromRoute] int id, [FromBody] RequestUpdateUserJson request)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult DeleteUser([FromRoute] int id)
+        {
+            return NoContent();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<User>), StatusCodes.Status200OK)]
+        public IActionResult GetAll()
+        {
+            var response = new List<User>()
+            {
+                new User {Id = 1, Email = "orranal", Name = "Orrana"},
+                new User {Id = 2, Email = "orranal", Name = "Orrana"}
+            };
+
+            return Ok(response);
+        }
+
+        [HttpPut("/changepassword")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult ChangePassword([FromBody] RequestChangePasswordJson request)
+        {
+            return NoContent();
+        }
+    }
+}
